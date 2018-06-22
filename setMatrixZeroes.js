e369a3b823b362b0566673359b49c644c5f22f10
ACODE91 <aaron.z.lao@gmail.com>
2018-06-21 19:36:12 -0700

    fixed setmatrixzero solution

--- setMatrixZeroes.js
+++ setMatrixZeroes.js
@@ -35,26 +35,33 @@
 // Could you devise a constant space solution?
 
 let setZeroes = function(matrix) {
-    let col0 = 1;
-    let rows = matrix.length;
-    let cols = matrix[0].length;
-
-    for(let i = 0; i < rows; i ++) {
-        if(matrix[i][0] == 0) {
-            col0 = 0;
-        }
-        for(let j = 1; j < cols; j++) {
+    let fr = false
+    let fc = false;
+    for(let i = 0; i < matrix.length; i++) {
+        for(let j = 0; j < matrix[0].length; j++) {
             if(matrix[i][j] == 0) {
-                matrix[i][0] = matrix[0][j] = 0;
+                if(i == 0) fr = true;
+                if(j == 0) fc = true;
+                matrix[0][j] = 0;
+                matrix[i][0] = 0;
             }
         }
     }
-
-    for(let i = rows - 1; i >= 0; i--) {
-        for(let j = cols - 1; j >= 1; j--) {
-            if (matrix[i][0] == 0 || matrix[0][j] == 0)
-            matrix[i][j] = 0;
-    if (col0 == 0) matrix[i][0] = 0;
+    for(let i = 1; i < matrix.length; i++) {
+        for(let j = 1; j < matrix[0].length; j++) {
+            if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    if(fr) {
+        for(let j = 0; j < matrix[0].length; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+    if(fc) {
+        for(let i = 0; i < matrix.length; i++) {
+            matrix[i][0] = 0;
         }
     }
 }
