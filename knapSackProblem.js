d4c31dafa23b649ccbef019ceffe31046b9a656e
ACODE91 <aaron.z.lao@gmail.com>
2018-07-06 12:31:59 -0700

    fixed knapsack problem

--- knapSackProblem.js
+++ knapSackProblem.js
@@ -1,3 +1,25 @@
+
+         // Returns the maximum value that can be put in a knapsack of capacity W
+            let knapSack = function(W, wt, val, n) {
+            let i, w;
+            let K = new Array(n+1);
+            K.fill(new Array(W + 1));
+            
+             for (i = 0; i <= n; i++)
+             {
+                 for (w = 0; w <= W; w++)
+                 {
+                     if (i==0 || w==0)
+                          K[i][w] = 0;
+                     else if (wt[i-1] <= w)
+                           K[i][w] = Math.max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]);
+                     else
+                           K[i][w] = K[i-1][w];
+                 }
+              }
+              
+              return K[n][W];
+            }
 //brute force
 let knapSackProblem = function(array, max) {
     let totalWeight = 0, index = 0, totalValue = 0, knapSack = [];
@@ -5,9 +27,10 @@ let knapSackProblem = function(array, max) {
     array.sort((a, b) => {
         return b.value - a.value;
     });
-    //debugger;
+
     for(let i = 0; i < array.length; i++) {
         if(totalWeight + array[i].weight < max) {
+            totalWeight += array[i].weight;
             knapSack.push(array[i]);
             index++;
             totalValue += array[i].value;
@@ -16,7 +39,7 @@ let knapSackProblem = function(array, max) {
         } 
     }
 
-    while(index !== array.length - 1) {
+    while(index <= array.length - 1) {
         for(let i = array.length - 1; i >= index; i--) {
             if(totalWeight + array[i].weight < max) {
                 knapSack.push(array[i]);
@@ -35,6 +58,3 @@ let knapSackProblem = function(array, max) {
     }
     return totalValue;
 }
-
-knapSackProblem([{weight: 1, value: 5}, {weight: 2, value: 3}, 
-{weight: 4, value: 5}, {weight: 2, value: 3}, {weight: 5, value: 2}], 10)
