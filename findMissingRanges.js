8157ba344054b5ae368affdff1c76a6c11caeb30
ACODE91 <aaron.z.lao@gmail.com>
2018-07-09 01:15:23 -0700

    added new solution to findMissingRanges

--- findMissingRanges.js
+++ findMissingRanges.js
@@ -1,3 +1,36 @@
+let findMissingRanges = function(nums, lower, upper) {
+    let results =[];
+  
+    // the next number we need to find
+    let next = lower;
+    
+    for (let i = 0; i < nums.length; i++) {
+      // not within the range yet
+      if (nums[i] < next) continue;
+      
+      // continue to find the next one
+      if (nums[i] == next) {
+        next++;
+        continue;
+      }
+      
+      // get the missing range string format
+      results.push(getRange(next, nums[i] - 1));
+      
+      // now we need to find the next number
+      next = nums[i] + 1;
+    }
+    
+    // do a final check
+    if (next <= upper) results.push(getRange(next, upper));
+  
+    return results;
+}
+
+let getRange = function(n1, n2) {
+    return (n1 == n2) ? n1.toString() : n1.toString() + '->' + n2.toString();
+}
+
 //brute force method
 var findMissingRanges = function(nums, lower, upper) {
     let lowerR = lower;
