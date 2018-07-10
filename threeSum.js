3729a376c1f3fbc46eedbf507e4f330b45f7d839
ACODE91 <aaron.z.lao@gmail.com>
2018-07-09 23:56:57 -0700

    cleaned up the code

--- threeSum.js
+++ threeSum.js
@@ -1,3 +1,23 @@
+let threeSum = function(nums) {
+    nums.sort((a,b) => {return a - b});
+    let res = [];
+    for (let i = 0; i < nums.length-2; i++) {
+        if (i == 0 || (i > 0 && nums[i] != nums[i-1])) {
+            let lo = i+1, hi = nums.length-1, sum = 0 - nums[i];
+            while (lo < hi) {
+                if (nums[lo] + nums[hi] == sum) {
+                    res.push([nums[i], nums[lo], nums[hi]]);
+                    while (lo < hi && nums[lo] == nums[lo+1]) lo++;
+                    while (lo < hi && nums[hi] == nums[hi-1]) hi--;
+                    lo++; hi--;
+                } else if (nums[lo] + nums[hi] < sum) lo++;
+                else hi--;
+           }
+        }
+    }
+    return res;
+}
+
 //brute force attempt
 var threeSum = function(nums) {
     let results = [];
