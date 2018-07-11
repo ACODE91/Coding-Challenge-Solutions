0c0b3d4eebd9e6554d421a9d10baff247207399c
ACODE91 <aaron.z.lao@gmail.com>
2018-07-10 21:19:22 -0700

    added binary tree level order traversal challenge;

--- twoSum.js
+++ twoSum.js
@@ -2,6 +2,8 @@ var twoSum = function(nums, target) {
     let map = {}
 
     for (i = 0; i < nums.length; i++) {
+        //if you ever run into problems that involve sums and you can't sort the order
+        //use something like complement and store the remaining values in map.
         let complement = target - nums[i];
         if (map[complement] !== undefined) {
             return [map[complement], i];
