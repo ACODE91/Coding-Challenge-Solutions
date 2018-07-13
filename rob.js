d367580cb12f632afad55b0373686a9056e9a84a
ACODE91 <aaron.z.lao@gmail.com>
2018-07-13 11:45:11 -0700

    added comment

--- rob.js
+++ rob.js
@@ -1,3 +1,5 @@
+//When considering challenges that tries to find the max value of something and constraints 
+//that limit your options, always use Math.max.
 let rob = function(num, n) {
     let prevMax = 0;
     let currMax = 0;
