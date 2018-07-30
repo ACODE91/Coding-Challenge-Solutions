7190240c619e73998a0c40c74aa91c0449d20ddc
ACODE91 <aaron.z.lao@gmail.com>
2018-07-30 02:18:46 -0700

    completed max depth of binary tree

---
+++ maxDepthBinaryTree.js
@@ -0,0 +1,48 @@
+//my approach
+let Queue = function() {
+    this.storage = [];
+}
+
+Queue.prototype.pop = function() {
+   return this.storage.shift();
+}
+
+Queue.prototype.isEmpty = function() {
+    if(this.storage.length == 0) {
+        return true;
+    }
+    return false;
+}
+
+var maxDepth = function(root) {
+ 
+    if(root === null) {
+        return 0;
+    }
+    
+    let count = 0;
+    let queue = new Queue();
+    queue.storage.push(root);
+
+    while(!queue.isEmpty()) {
+        let n = queue.storage.length;
+
+        for(let i = 0; i < n; i++) {
+            if(queue.storage[i].left) {
+                queue.storage.push(queue.storage[i].left);
+            } 
+            if(queue.storage[i].right) {
+                queue.storage.push(queue.storage[i].right);
+            }
+        }
+
+        while(n > 0) {
+          queue.pop();
+          n--;
+        }
+
+        count++;
+    }
+
+    return count;
+};
