7ffe33a7fe17d28fc1759e95890137fc831f9d25
ACODE91 <aaron.z.lao@gmail.com>
2018-08-15 21:25:26 -0700

    added lowest common ancester challenge

--- triangle.js
+++ triangle.js
@@ -6,6 +6,13 @@ let minimumTotal = function(triangle) {
         for (let i = 0; i <= layer; i++) // Check its every 'node'
         {
             // Find the lesser of its two children, and sum the current value in the triangle with it.
+            
+            //for each element, you're choosing the least value so it keeps swapping 
+            //(by overwriting minlen[i])
+            //and that is why you only need to return the first element.
+            //since your constraint is that you could only pick adjacent elements
+            //it would make sense to iterate from left to right after picking the lesser 
+            //element and adding each layer to the lesser element.
             minlen[i] = Math.min(minlen[i], minlen[i+1]) + triangle[layer][i]; 
         }
     }
