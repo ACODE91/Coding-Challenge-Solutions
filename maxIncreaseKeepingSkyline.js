f7369322e171dc769ce86359d1f848a9186ebc56
ACODE91 <aaron.z.lao@gmail.com>
2018-08-31 16:50:10 -0700

    implemented cleaner code

--- maxIncreaseKeepingSkyline.js
+++ maxIncreaseKeepingSkyline.js
@@ -1,29 +1,18 @@
 var maxIncreaseKeepingSkyline = function(grid) {
-    var xMax = [];
-    var yMax = [];
-    var leni = grid.length;
-    var result = 0;
-    for (var i = 0; i < leni; i++) {
-        var lenj = grid[i].length;
-        if (typeof xMax[i]=== 'undefined') {
-            xMax[i] = grid[i][0];
-            for (var k = 1; k < lenj; k++) {
-                if (grid[i][k] > xMax[i]) {
-                    xMax[i] = grid[i][k];
-                }
+
+    let rowMaxes = new Array(grid[0].length), colMaxes = new Array(grid[0].length), N = grid.length;
+    rowMaxes.fill(0);
+    colMaxes.fill(0);
+            for (let r = 0; r < N; ++r)
+                for (let c = 0; c < N; ++c) {
+                    rowMaxes[r] = Math.max(rowMaxes[r], grid[r][c]);
+                    colMaxes[c] = Math.max(colMaxes[c], grid[r][c]);
             }
-        } 
-        for (var j = 0; j < lenj; j++) {
-            if (typeof yMax[j] === 'undefined') {
-                yMax[j] = grid[0][j];
-                for (var l = 1; l < leni; l++) {
-                    if (grid[l][j] > yMax[j]) {
-                        yMax[j] = grid[l][j];
-                    }
-                }
-            }
-            result += ((xMax[i] > yMax[j] ? yMax[j] : xMax[i]) - grid[i][j]);
-        }
+            let ans = 0;
+            for (let r = 0; r < N; ++r)
+                for (let c = 0; c < N; ++c)
+                    ans += Math.min(rowMaxes[r], colMaxes[c]) - grid[r][c];
+    
+            return ans;
+     
     }
-    return result;
-};
