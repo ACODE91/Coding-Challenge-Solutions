db4d775217119b0167e920775a724a347b6a4caf
ACODE91 <aaron.z.lao@gmail.com>
2018-09-24 15:54:18 -0700

    added naive solution for replaceWords

---
+++ replaceWords.js
@@ -0,0 +1,29 @@
+let replaceWords = function(dict, sentence) {
+    let result = "";
+    sentence = sentence.split(' ');
+    dictObj = {};
+
+    for(let i = 0; i < dict.length; i++) {
+        dictObj[dict[i]] = 1;
+    }
+
+    for(let i = 0; i < sentence.length; i++) {
+        let index = 0;
+        let curr = "";
+        while(curr.length < sentence[i].length) {
+            curr+= sentence[i][index];
+            if(dictObj[curr]) {
+                break;
+            }
+            index++;
+        }
+
+        if(i == sentence.length - 1) {
+            result += curr
+        } else {
+                 result += curr + ' ';   
+        }
+    }
+
+    return result;
+}
