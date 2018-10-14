aba06a0efc1c541883657639db547daa5c86135a
ACODE91 <aaron.z.lao@gmail.com>
2018-10-14 13:47:14 -0700

    updated findAnagrams solution

--- findAnagrams.js
+++ findAnagrams.js
@@ -1,27 +1,33 @@
-let findAnagrams = (s, p) => {
-	let list = [];
-	if (s == null || s.length == 0 || p == null || p.length == 0) return list;
-	let hash = {}; //character hash
-	//record each character in p to hash
-	for (let i = 0; i < p.length; i++) {
-		hash[p[i]]++;
+var findAnagrams = function(s, p) {
+	var map = {};
+	var result = [];
+	var slideWindow = {};
+	for(var i = 0; i < p.length; i++){
+		map[p[i]] = map[p[i]] ? map[p[i]] + 1 : 1;
 	}
-	//two points, initialize count to p's length
-	let left = 0, right = 0, count = p.length;
-	while (right < s.length) {
-		//move right everytime, if the character exists in p's hash, decrease the count
-		//current hash value >= 1 means the character is existing in p
-		if (hash[s[right++]]-- >= 1) count--; 
-    
-		//when the count is down to 0, means we found the right anagram
-		//then add window's left to result list
-		if (count == 0) list.push(left);
-
-		//if we find the window's size equals to p, then we have to move left (narrow the window) to find the new match window
-		//++ to reset the hash because we kicked out the left
-		//only increase the count if the character is in p
-		//the count >= 0 indicate it was original in the hash, cuz it won't go below 0
-		if (right - left == p.length && hash[s[left++]]++ >= 0) count++;
+	// two pointers here: the characters in between are anagrams needed
+	var left = 0;
+	var right = 0;
+	while(right < s.length){
+		//
+		if(map[s.charAt(right)]){
+		    // NOTICE: we only need to update left for non-continuous duplicated character,
+			// because the following "if" will automatically handle the situation: "baa" "aa"
+			if(slideWindow[s.charAt(right)] && map[s.charAt(right)] === 1){
+				left = Math.max(slideWindow[s.charAt(right)], left);
+			}
+			if(right - left + 1 === p.length){
+				result.push(left);
+				left++;
+			}
+		}else{
+			left = right + 1;
+			slideWindow = {};
+		}
+		// the core of sliding window: need to save next index to current key,
+		// so we can leverage it whenever we meet this key again in the future.
+		slideWindow[s.charAt(right)] = right + 1;
+		right++;
 	}
-	return list;
+	return result;
 };
