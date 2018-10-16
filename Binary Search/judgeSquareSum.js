ecab0d73175da424e62d5379fc7baa41a9491606
ACODE91 <aaron.z.lao@gmail.com>
2018-10-16 14:09:22 -0700

    added judgeSquareSum solution

---
+++ Binary Search/judgeSquareSum.js
@@ -0,0 +1,18 @@
+let judgeSquareSum = function(c) {
+	for (let a = 0; a * a <= c; a++) {
+		let b = c - (a * a);
+		if (binary_search(0, b, b))
+			return true;
+	}
+	return false;
+};
+let binary_search = function(s, e, n) {
+	if (s > e)
+		return false;
+	let mid = s + (e - s) / 2;
+	if (mid * mid == n)
+		return true;
+	if (mid * mid > n)
+		return binary_search(s, mid - 1, n);
+	return binary_search(mid + 1, e, n);
+};
