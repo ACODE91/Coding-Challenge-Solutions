f380f051de747d2e74946b056cc4216ccce66414
ACODE91 <aaron.z.lao@gmail.com>
2018-10-25 14:44:27 -0700

    added first solution to moveZeroes

---
+++ Queues/moveZeroes.js
@@ -0,0 +1,21 @@
+//first solution with queue
+var moveZeroes = function(nums) {
+	let queue = [];
+
+	for(let i = 0; i < nums.length; i++) {
+		if(nums[i] == 0) {
+			queue.push(i);
+		} else {
+			if(queue.length > 0) {
+				let tmp = nums[i];
+				nums[i] = nums[queue[0]];
+				nums[queue[0]] = tmp;
+				queue.shift();
+				queue.push(i);
+			} else {
+				continue;
+			}
+
+		}
+	}
+};
