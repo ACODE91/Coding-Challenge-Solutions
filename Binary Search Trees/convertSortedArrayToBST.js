e21df6b034b15e9cbe0cc1503fa78f1766903021
ACODE91 <aaron.z.lao@gmail.com>
2018-11-04 11:07:31 -0800

    added alt solution to convert sorted array to BST

--- Binary Search Trees/convertSortedArrayToBST.js
+++ Binary Search Trees/convertSortedArrayToBST.js
@@ -1,74 +1,49 @@
 let Node = function(val) {
-    this.val = val;
-    this.left = this.right = null;
-}
-
-let Stack = function() {
-    this.storage = [];
-}
-
-Stack.prototype.push = function(item) {
-    this.storage.push(item);
-}
-
-Stack.prototype.pop = function() {
-    return this.storage.pop();
-}
-
-Stack.prototype.isEmpty = function() {
-    if(this.storage.length > 0) {
-        return false;
-    }
-    return true;
-}
-
-var sortedArrayToBST = function(nums) {
-  let mid = Math.floor(nums.length / 2), left = mid - 1, right = nums.length - 1;
-  let root = new Node(nums[mid]), ans = new Stack();
-  ans.push(nums[mid]);
-    
-  while(left > -1 && right > mid) {
-      let leftNode = new Node(nums[left]);
-      let rightNode = new Node(nums[right]);
-      helper(root, leftNode);
-      helper(root, rightNode);
-      right--;
-      left--;
-  }
-    
-    if(left > 0) {
-        helper(nums[mid], left);
-    } else if(right > mid) {
-        helper(nums[mid], right);
-    }
-    
-    let currNode = root;
-    
-    while(currNode.left || currNode.right) {
-        stack.push(currNode.left);
-        stack.push(currNode.right);
-    }
+	this.val = val;
+	this.left = this.right = null;
 };
 
-let traverse = function(node, l, r, arr) {
-    if(!l && !r) {
-        return arr;
-    }
+var sortedArrayToBST = function(nums) {
+	let mid = Math.floor(nums.length / 2), left = mid - 1, right = nums.length - 1;
+	let root = new Node(nums[mid]);
     
-    arr.push(node);
-    if(l) {
-        traverse(l, )
-    }
-}
+	while(left > -1 && right > mid) {
+		let leftNode = new Node(nums[left]);
+		let rightNode = new Node(nums[right]);
+		helper(root, leftNode);
+		helper(root, rightNode);
+		right--;
+		left--;
+	}
+    
+	if(left > 0) {
+		helper(nums[mid], left);
+	} else if(right > mid) {
+		helper(nums[mid], right);
+	}
+    
+	return root;
+};
+
 
 let helper = function(root, node) {
-    if(!root.left && node.val < root.val) {
-        root.left = node;
-    } else if(!root.right && node.val > root.val) {
-        root.right = node;
-    } else if(root.left && node.val < root.val) {
-        helper(root.left, node)
-    } else if (root.right && node.val > root.val) {
-        helper(root.right, node);
-    }
-}
+	if(!root.left && node.val < root.val) {
+		root.left = node;
+	} else if(!root.right && node.val > root.val) {
+		root.right = node;
+	} else if(root.left && node.val < root.val) {
+		helper(root.left, node);
+	} else if (root.right && node.val > root.val) {
+		helper(root.right, node);
+	}
+};
+
+//alt solution
+var sortedArrayToBST = function(nums) {
+	if(!nums.length) return null;   
+	var mid = Math.floor((nums.length)/2);
+	var root = new TreeNode(nums[mid]);
+	root.left = sortedArrayToBST(nums.slice(0, mid));
+	root.right = sortedArrayToBST(nums.slice(mid+1));
+	return root;
+};
