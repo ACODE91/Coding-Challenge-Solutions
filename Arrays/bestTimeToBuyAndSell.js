5484846b1a8ef6e57f7f89d285546df8d24e694a
Aaron Lao <acode91>
2019-01-28 22:23:30 -0800

    added maxProfit solution

---
+++ Arrays/bestTimeToBuyAndSell.js
@@ -0,0 +1,9 @@
+var maxProfit = function(prices) {
+  let maxCur = 0,
+    maxSoFar = 0;
+  for (let i = 1; i < prices.length; i++) {
+    maxCur = Math.max(0, (maxCur += prices[i] - prices[i - 1]));
+    maxSoFar = Math.max(maxCur, maxSoFar);
+  }
+  return maxSoFar;
+};
