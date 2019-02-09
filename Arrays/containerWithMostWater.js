9353c5140400ffa1a0a506eb515493cfba104bb3
Aaron Lao <acode91>
2019-02-08 20:04:47 -0800

    added container with most water solution

---
+++ Arrays/containerWithMostWater.js
@@ -0,0 +1,16 @@
+var maxArea = function(height) {
+  let left = 0,
+    right = height.length - 1,
+    max = 0;
+  while (left < right) {
+    max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
+
+    if (height[left] > height[right]) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+
+  return max;
+};
